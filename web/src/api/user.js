e37b99f603a74ef55bc8826818666d647612c3d1
kubeops <kubeops@163.com>
2022-01-20 17:56:55 +0800

    前端更新

--- web/src/api/user.js
+++ web/src/api/user.js
@@ -127,6 +127,22 @@ export const setUserAuthorities = (data) => {
   })
 }
 
+// @Tags SysUser
+// @Summary 设置用户信息
+// @Security ApiKeyAuth
+// @accept application/json
+// @Produce application/json
+// @Param data body model.SysUser true "设置用户信息"
+// @Success 200 {string} string "{"success":true,"data":{},"msg":"修改成功"}"
+// @Router /user/setSelfInfo [put]
+export const setSelfInfo = (data) => {
+  return service({
+    url: '/user/setSelfInfo',
+    method: 'put',
+    data: data
+  })
+}
+
 // @Tags User
 // @Summary 获取用户信息
 // @Security ApiKeyAuth
