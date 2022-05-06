a90bbc5203664be4582463d168460140e0e2d404
kubeops <kubeops@163.com>
2022-05-06 11:14:48 +0800

    geJobList分页优化

--- server/service/kubernetes/job/list.go
+++ server/service/kubernetes/job/list.go
@@ -4,33 +4,32 @@ import (
 	"context"
 	"fmt"
 	resourceJob "github.com/pddzl/kubeops/server/model/kubernetes/resource/job"
-	batchv1 "k8s.io/api/batch/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	batchV1 "k8s.io/api/batch/v1"
+	metaV1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/pddzl/kubeops/server/global"
 	"github.com/pddzl/kubeops/server/model/common/request"
 )
 
 func (j *JobService) GetJobList(namespace string, info request.PageInfo) ([]resourceJob.JobBrief, int, error) {
-	// 获取job原生数据
-	jobs, err := global.KOP_KUBERNETES.BatchV1().Jobs(namespace).List(context.TODO(), metav1.ListOptions{})
+	// 获取job list
+	list, err := global.KOP_KUBERNETES.BatchV1().Jobs(namespace).List(context.TODO(), metaV1.ListOptions{})
 	if err != nil {
 		return nil, 0, err
 	}
 
+	var jobList batchV1.JobList
+	// 分页
 	end := info.PageSize * info.Page
 	offset := info.PageSize * (info.Page - 1)
-	var jobList batchv1.JobList
-
-	// 分页
-	total := len(jobs.Items)
+	total := len(list.Items)
 	if total <= offset {
 		return nil, total, nil
 	}
 	if total < end {
-		jobList.Items = jobs.Items[offset:]
+		jobList.Items = list.Items[offset:]
 	} else {
-		jobList.Items = jobs.Items[offset:end]
+		jobList.Items = list.Items[offset:end]
 	}
 
 	// 处理jobs数据
